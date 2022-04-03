36044ca24591ca9d0f4433bbf0b67b659de983ff
Ikechukwu Kalu <ikechukwuakalu@gmail.com>
2022-04-03 16:12:11 +0100

    refactor: restructure folders

--- scripts/cloud-providers/ibm/speech-to-text.py
+++ scripts/cloud-providers/ibm/speech-to-text.py
@@ -1,9 +1,6 @@
 # install ibm_watson package
 # !pip install ibm_watson wget
 
-# download audio file
-# !wget -O PolynomialRegressionandPipelines.mp3  https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0101EN-SkillsNetwork/labs/Module%205/data/PolynomialRegressionandPipelines.mp3
-
 
 from ibm_watson import SpeechToTextV1 
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
